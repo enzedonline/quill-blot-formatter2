fc9d05f9ceb1427223dc356ca462f6e513671025
rich <richard@enzedonline.com>
2024-06-29 14:09:11 +1200

    commented original config

--- webpack.config.js
+++ webpack.config.js
@@ -36,3 +36,17 @@ module.exports = {
     }),
   ],
 };
+
+// original config
+// module.exports = {
+//   entry: './dist/index.js',
+//   output: {
+//     filename: 'quill-blot-formatter.min.js',
+//     path: `${__dirname}/dist`,
+//     libraryTarget: 'umd',
+//     library: 'QuillBlotFormatter',
+//   },
+//   externals: {
+//     quill: 'Quill',
+//   },
+// };
