05044e6db5a10aba9515dd1c146cd11ea2d216c4
rich <richard@enzedonline.com>
2024-08-01 16:14:26 +1200

    custom image blot to handle title attribute

---
+++ src/blots/Image.ts
@@ -0,0 +1,32 @@
+import Quill from 'quill';
+const ImageBlot = Quill.import('formats/image') as any;
+
+const ATTRIBUTES = ['alt', 'height', 'width', 'title'];
+
+class Image extends ImageBlot {
+    static formats(domNode: Element) {
+        return ATTRIBUTES.reduce(
+            (formats: Record<string, string | null>, attribute) => {
+                if (domNode.hasAttribute(attribute)) {
+                    formats[attribute] = domNode.getAttribute(attribute);
+                }
+                return formats;
+            },
+            {},
+        );
+    }
+
+    format(name: string, value: string) {
+        if (ATTRIBUTES.indexOf(name) > -1) {
+            if (value) {
+                this.domNode.setAttribute(name, value);
+            } else {
+                this.domNode.removeAttribute(name);
+            }
+        } else {
+            super.format(name, value);
+        }
+    }
+}
+
+export default Image;
