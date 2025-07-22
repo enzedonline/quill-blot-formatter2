75c1ba8ebd3b4399a64c32dc67285cf671e9fec4
rich <richard@enzedonline.com>
2025-07-22 18:40:43 +1200

    Remove passive attribute from event listeners in DeleteAction. Allows child elements to stop propogation.

--- src/actions/DeleteAction.ts
+++ src/actions/DeleteAction.ts
@@ -3,13 +3,13 @@ import Action from './Action';
 
 export default class DeleteAction extends Action {
   onCreate() {
-    document.addEventListener('keyup', this.onKeyUp, true);
-    this.formatter.quill.root.addEventListener('input', this.onKeyUp, true);
+    document.addEventListener('keyup', this.onKeyUp);
+    this.formatter.quill.root.addEventListener('input', this.onKeyUp);
   }
 
   onDestroy() {
-    document.removeEventListener('keyup', this.onKeyUp, true);
-    this.formatter.quill.root.removeEventListener('input', this.onKeyUp, true);
+    document.removeEventListener('keyup', this.onKeyUp);
+    this.formatter.quill.root.removeEventListener('input', this.onKeyUp);
   }
 
   onKeyUp = (e: KeyboardEvent) => {
