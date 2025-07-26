64e38ac078668919164811c71b2da38f56703532
rich <richard@enzedonline.com>
2025-07-27 11:50:16 +1200

    Export LinkAction & CaretAction

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,13 @@ export { default as AlignAction } from './actions/align/AlignAction';
 export { default as DefaultAligner } from './actions/align/DefaultAligner';
 export { default as DeleteAction } from './actions/DeleteAction';
 export { default as ResizeAction } from './actions/ResizeAction';
-export { default as AttributeAction} from './actions/AttributeAction'
+export { default as AttributeAction} from './actions/AttributeAction';
+export { default as LinkAction} from './actions/LinkAction';
+export { default as CaretAction} from './actions/CaretAction';
 
 // toolbar
-export {default as Toolbar} from './actions/toolbar/Toolbar'
-export {default as ToolbarButton} from './actions/toolbar/ToolbarButton'
+export {default as Toolbar} from './actions/toolbar/Toolbar';
+export {default as ToolbarButton} from './actions/toolbar/ToolbarButton';
 
 // specs
 export { default as BlotSpec } from './specs/BlotSpec';
